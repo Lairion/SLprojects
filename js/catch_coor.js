f2d88ed9f9d47c35354648fe415a975afbfd84db
Manuel Antonio <nunyes_m_a@ukr.net>
2016-07-06 20:45:03 +0300

    Selection image

--- js/catch_coor.js
+++ js/catch_coor.js
@@ -1,5 +1,12 @@
-$(document).ready(
-    $('#current-img').click(
-        $('#sel-blok').css({left:})
-    )
-)
+var curntImg = $('#current-img').imgAreaSelect({
+        handles : true,
+        borderColor1 : "#006400",
+        borderColor2 : "#20B2AA",
+        borderOpacity : "1",
+        outerColor : "#FFD700",
+        outerOpacity : "1",
+        selectionColor: '#00FF00',
+        selectionOpacity: '0.5'
+    });
+
+
