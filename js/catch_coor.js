0878c3da9dfff48c1f97c604b7f988e47502a1a2
Anton <Anton>
2016-07-07 21:29:45 +0300

    Added input

--- js/catch_coor.js
+++ js/catch_coor.js
@@ -1,3 +1,21 @@
+var x1;
+var y1;
+var x2;
+var y2;
+function getCoordin(img,selection){
+    x1 = $("#X1").attr("value", selection.x1)
+    y1 = $("#Y1").attr("value", selection.y1)
+    x2 = $("#X2").attr("value", selection.x2)
+    y2 = $("#Y2").attr("value", selection.y2)
+}
+function addOk(){
+    $("#img-ok").html("<button>OK</button>")
+}
+
+function inputCoordinate(img,selection){
+
+}
+
 var curntImg = $('#current-img').imgAreaSelect({
         handles : true,
         borderColor1 : "#DCDCDC",
@@ -6,7 +24,10 @@ var curntImg = $('#current-img').imgAreaSelect({
         outerColor : "null",
         outerOpacity : "0",
         selectionColor: '#F5F5F5',
-        selectionOpacity: '0.2'
+        selectionOpacity: '0.5',
+        onSelectStart:addOk,
+        onSelectChange : getCoordin
+
     });
 
 
